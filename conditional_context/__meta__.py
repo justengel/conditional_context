db75ed7b1b93745ae325658db5caef8b17d0d198
justengel <jtengel08@gmail.com>
2020-09-29 12:25:03 -0400

    Updated to version 1.0.2

--- conditional_context/__meta__.py
+++ conditional_context/__meta__.py
@@ -1,5 +1,5 @@
 name = 'conditional_context'
-version = '1.0.1'
+version = '1.0.2'
 description = 'Context manager that will skip the body on a condition.'
 url = 'https://github.com/justengel/conditional_context'
 author = 'Justin Engel'
